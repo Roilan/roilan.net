20e96f22d5874f0ea2a606ae1c11c1dfe8e3aafb
Roilan <roilan14@gmail.com>
2014-06-05 12:57:51 -0400

    Portfolio using lightbox and removed instagram from icon links

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
-  var $footerAbout = $('#footer-about'),
+  var $footerPortfolio = $('#footer-portfolio'),
+      $footerAbout = $('#footer-about'),
       $footerContact = $('#footer-contact'),
       $footerSent = $('#footer-contact-sent'),
+      $clickPortfolio = $('#clickPortfolio'),
       $clickAbout = $('#clickAbout'),
       $clickContact = $('#clickContact'),
       $arrowImg = $('#footer-start img'),
       $arrowTxt = $('#footer-start span'),
       sentUrl = window.location.href;
 
+  $footerPortfolio.hide();
   $footerSent.hide();
   $footerAbout.hide();
   $footerContact.hide();
@@ -19,14 +22,23 @@ $(document).ready(function() {
       $arrowTxt.hide('fade');
     });
 
+  $clickPortfolio.click(function() {
+    event.preventDefault();
+    $footerAbout.slideUp(500);
+    $footerContact.slideUp(600);
+    $footerPortfolio.slideToggle(500);
+  });
+
   $clickAbout.click(function() {
     event.preventDefault();
+    $footerPortfolio.slideUp(500);
     $footerContact.slideUp(500);
     $footerAbout.slideToggle(500);
   });
 
   $clickContact.click(function() {
     event.preventDefault();
+    $footerPortfolio.slideUp(500);
     $footerAbout.slideUp(500);
     $footerContact.slideToggle(500);
   });
@@ -36,9 +48,17 @@ $(document).ready(function() {
     $footerSent.slideDown(800).delay(1000).slideUp(800);
   }
 
-  $('.lightbox').magnificPopup({ 
+  $('.lightbox').magnificPopup({
     type: 'image',
-    closeOnContentClick: true
+    closeBtnInside: false,
+    closeOnContentClick: true,
+
+    image: {
+      verticalFit: true,
+      titleSrc: function(item) {
+        return item.el.attr('title');
+      }
+    }
   });
 
 });
